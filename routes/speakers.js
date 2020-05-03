75155a18c1de66727bf8c0d45e34336177f0ed79
Hari Ilangovan <h.ilangovan@gmail.com>
2020-05-03 11:09:39 -0400

    adding dynamic updates to speaker pages

--- routes/speakers.js
+++ routes/speakers.js
@@ -7,11 +7,14 @@ module.exports = (params) => {
 
   router.get('/', async (request, response) => {
     const speakers = await speakersService.getList();
-    return response.json(speakers);
+    response.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers });
   });
 
-  router.get('/:shortname', (request, response) => {
-    return response.send(`Detail page of ${request.params.shortname}`);
+  router.get('/:shortname', async (request, response) => {
+    const speaker = await speakersService.getSpeaker(request.params.shortname);
+    console.log(speaker);
+    response.render('layout', { pageTitle: 'Speakers', template: 'speakers-detail', speaker });
+    // return response.send(`Detail page of ${request.params.shortname}`);
   });
 
   return router;
